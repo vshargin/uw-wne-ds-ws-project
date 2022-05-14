6c49776836ab23cd3d0435082869f98d1316c8e5
Vladimir Shargin <me@vshargin.ru>
2022-05-14 23:47:27 +0200

    Fix Scrapy

--- scrapy/scrapy1/spiders/scrapy3.py
+++ scrapy/scrapy1/spiders/scrapy3.py
@@ -1,21 +1,22 @@
 import scrapy
+from scrapy.exceptions import CloseSpider
+
+limit = True
 
 class Table(scrapy.Item):
-    Pos = scrapy.Field()
-    No = scrapy.Field()
-    Driver = scrapy.Field()
-    Car = scrapy.Field()
-    LAPS = scrapy.Field()
-    TIME = scrapy.Field()
+    SEASON = scrapy.Field()
+    RACE_NAME = scrapy.Field()
+    FULL_RACE_NAME = scrapy.Field()
+    RACE_DATE = scrapy.Field()
+    RACE_LOCATION = scrapy.Field()
+    POS = scrapy.Field()
+    DRIVER = scrapy.Field()
+    CAR = scrapy.Field()
     PTS = scrapy.Field()
-    Location = scrapy.Field()
-    Race_name = scrapy.Field()
-    Date = scrapy.Field()
-    Season = scrapy.Field()
-    Place = scrapy.Field()
 
 #creating the class Spider for scapry
 class LinksSpider(scrapy.Spider):
+    scraped_pages = 0
     name = 'data'
     allowed_domains = ['formula1.com']
     try:
@@ -27,28 +28,41 @@ class LinksSpider(scrapy.Spider):
 
     # get the data about races, driver, places, result and other additional info
     def parse(self, response):
-        t = Table()
+        if limit and self.scraped_pages >= 100:
+            raise(CloseSpider)
+        Location1 = 'normalize-space(//span[@class="circuit-info"])'
+        Race_name1 = 'normalize-space(//h1[@class="ResultsArchiveTitle"])'
+        Date1 = 'normalize-space(//span[@class="full-date"])'
+        Season1 = 'normalize-space(// a[@data-name = "year"])'
+        Place1 = 'normalize-space(//a[@data-name="meetingKey"])'
+        location = response.xpath(Location1).get()
+        race_name = response.xpath(Race_name1).get()
+        date = response.xpath(Date1).get()
+        season = response.xpath(Season1).get()
+        track_name = response.xpath(Place1).get()
 
         Pos1 = '//tbody//tr//td[@class="dark"]/text()'
-        No1 = '//tbody//tr//td[@class="dark hide-for-mobile"]/text()'
         Driver1 = '//span[@class="hide-for-mobile"]/text()'
         Car1 = '//tbody//tr//td[@class="semi-bold uppercase hide-for-tablet"]/text()'
         PTS1 = '//tbody//tr//td[@class="bold"]/text()'
-        Location1 = '//span[@class="circuit-info"]/text()'
-        Race_name1 = '//h1[@class="ResultsArchiveTitle"]/text()'
-        Date1 = '//span[@class="full-date"]/text()'
-        Season1 = '// a[@data-name = "year"]/text()'
-        Place1 = '//a[@data-name="meetingKey"]/text()'
 
-        t['Pos'] = response.xpath(Pos1).getall()
-        t['No'] = response.xpath(No1).getall()
-        t['Driver'] = response.xpath(Driver1).getall()
-        t['Car'] = response.xpath(Car1).getall()
-        t['PTS'] = response.xpath(PTS1).getall()
-        t['Location'] = response.xpath(Location1).getall()
-        t['Race_name'] = response.xpath(Race_name1).getall()
-        t['Date'] = response.xpath(Date1).getall()
-        t['Season'] = response.xpath(Season1).getall()
-        t['Place'] = response.xpath(Place1).getall()
+        Pos = response.xpath(Pos1).getall()
+        Driver = response.xpath(Driver1).getall()
+        Car = response.xpath(Car1).getall()
+        PTS = response.xpath(PTS1).getall()
 
-        yield t
+        for i in range(len(Pos)):
+            t = Table()
+            t['SEASON'] = season
+            t['RACE_NAME'] = location
+            t['FULL_RACE_NAME'] = race_name
+            t['RACE_DATE'] = date
+            t['RACE_LOCATION'] = track_name
+            t['POS'] = Pos[i]
+            t['DRIVER'] = Driver[i]
+            t['CAR'] = Car[i]
+            t['PTS'] = PTS[i]
+            yield t
+
+        self.scraped_pages += 1
+        
